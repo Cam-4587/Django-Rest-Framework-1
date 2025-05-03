1536727bf817d933f0dd86d40e0a8d3e03855053
Cam-4587 Conroy <cameronconroy@live.com>
2025-05-03 20:26:51 +0000

    Adding local host to allowed hosts to make basic.py function properly

--- drf/backend/cfehome/settings.py
+++ drf/backend/cfehome/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1!yadv#0n@u#!@j3s0^yq8bfwfb-nv-*qh+3vtemv&oyprv*gj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.gitpod.io']
+ALLOWED_HOSTS = ['.gitpod.io', 'localhost']
 
 
 # Application definition
