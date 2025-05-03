c0686dae394a011fa625ae70a45a50c8dbaa3d37
Cam-4587 Conroy <cameronconroy@live.com>
2025-05-03 21:02:16 +0000

    finish Echo GET Data

--- drf/py_client/basic.py
+++ drf/py_client/basic.py
@@ -3,12 +3,12 @@ import requests
 
 endpoint = "http://localhost:8000/api/"
 
-get_response = requests.get(endpoint, json={"query": "Hello World"})
+get_response = requests.get(endpoint, params={"abc": 123},  json={"query": "Hello World"})
 
 print(get_response.text)
-print(get_response.status_code)
+# print(get_response.status_code)
 
-print(get_response.json()['message'])
+# print(get_response.json()['message'])
 
 # # Define the endpoint for a 200 status code
 # endpoint = "http://httpbin.org/status/200"  # This URL returns a 200 status code
